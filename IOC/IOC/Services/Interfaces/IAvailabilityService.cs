46cdb7a5398a831a7efd2d3a84168e7c111f1c5c
Daniela <daniela.popita9@gmail.com>
2022-12-23 21:31:59 +0000

    Initial Commit

---
+++ IOC/IOC/Services/Interfaces/IAvailabilityService.cs
@@ -0,0 +1,17 @@
+﻿using IOC.Models;
+using IOC.RequestModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IOC.Services.Interfaces
+{
+    public interface IAvailabilityService
+    {
+        Task<List<Availability>> GetAllAvailabilities();
+
+        Task<Availability> GetAvailability(int id);
+
+        Task<int> AddAvailability(CreateAvailabilityRequest createAvailabilityRequest);
+
+        Task<bool> DeleteAvailability(int id);
+    }
+}
