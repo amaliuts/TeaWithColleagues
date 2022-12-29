a2096c6a9f3dc8fc1b2c43ae3e6b0e658c9e2d0e
amaliuts <ami24amalia@gmail.com>
2022-12-29 18:42:56 +0200

    trial

---
+++ IOC/IOC/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace IOC.Models
+{
+    public class User
+    {
+        int idUser { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MailAddress { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Availability> Availabilities { get; } = new List<Availability>();
+    }
+}
